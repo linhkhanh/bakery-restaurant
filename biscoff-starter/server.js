e0ba7b11bc03f88627658f01bc515ad64f404894
Linh Le <linhle@Hieus-MacBook-Air.local>
2020-05-16 17:15:58 +0800

    Use heroku port

---
+++ biscoff-starter/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use((req, res, next) => {
+  console.log('I run for all routes');
+  next();
+});
+
+require('./routes')(app);
+
+app.listen(port, () => {
+  console.log(`Biscoff Bakery app listening on port: ${port}`)
+});
